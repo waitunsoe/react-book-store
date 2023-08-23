4a75bb03a429fa9808f2fa54469b91166ef16c2e
waitunsoe <waitunsoe.dev@gmail.com>
2023-08-23 15:28:11 +0630

    router installation & setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
